e08364b6680bdb53a4b950d2158fcc28a16f1e02
Legoshi <entityparrot@qq.com>
2022-11-03 11:27:50 +0800

    GPU memory summary updated

--- mem_summary.py
+++ mem_summary.py
@@ -2,3 +2,6 @@ import torch
 
 with open('memory_summary.txt', 'w') as file:
     file.write(torch.cuda.memory_summary())
+
+with open('memory_stats.txt', 'w') as file:
+    file.write(str(torch.cuda.memory_stats()))
