fa45ec170e0ebb2ca9fc1ebba5557bab8a6db7df
Legoshi <entityparrot@qq.com>
2022-11-25 15:20:30 +0800

    Update

--- experiment 2/net/__init__.py
+++
@@ -1,4 +0,0 @@
-from .resnet18 import ResNet18
-from .mobilenetv2 import MobileNetV2
-from .xception import Xception
-from .deeplabv3plus import DeepLabV3Plus
---
+++ experiment2/net/__init__.py
@@ -0,0 +1,4 @@
+from .deeplabv3plus import DeepLabV3Plus
+from .mobilenetv2 import MobileNetV2
+from .resnet18 import ResNet18
+from .xception import Xception
